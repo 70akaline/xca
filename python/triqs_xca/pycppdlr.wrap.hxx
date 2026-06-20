a1565dacf2f75493c69433d4dc235ddf8f799166
70akaline <36015241+70akaline@users.noreply.github.com>
2026-06-20 22:44:37 +0800

    Fix cppdlr c2py wrapper declarations

--- python/triqs_xca/pycppdlr.wrap.hxx
+++ python/triqs_xca/pycppdlr.wrap.hxx
@@ -4,8 +4,4 @@
 #define C2PY_HXX_DECLARATION_pycppdlr_GUARDS
 template <> constexpr bool c2py::is_wrapped<cppdlr::imtime_ops>     = true;
 template <> inline constexpr auto c2py::tp_name<cppdlr::imtime_ops> = "triqs_xca.pycppdlr.ImTimeOps";
-template <> constexpr bool c2py::is_wrapped<cppdlr::statistic_t>    = true;
-template <>
-const std::map<cppdlr::statistic_t, str_t> c2py::enum_to_string<cppdlr::statistic_t> = {{cppdlr::statistic_t::Boson, "Boson"},
-                                                                                        {cppdlr::statistic_t::Fermion, "Fermion"}};
 #endif
